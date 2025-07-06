3143dfea40d37fe13e93e8ccc78175a8ee6aee2a
Aseel Hassan <aseelha106@gmail.com>
2025-07-06 13:58:48 +0300

    add meta pixel

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { AboutUsPage } from "./pages/aboutUs.page";
 import { HomePage } from "./pages/home.page";
 import ScrollToTop from "./components/scrollTop.component";
 import { ProductsPage } from "./pages/products.page";
+import { Helmet } from "react-helmet";
 
 function App() {
   const { i18n } = useTranslation();
@@ -27,8 +28,34 @@ function App() {
     document.documentElement.lang = i18n.language;
   }, [i18n.language]);
 
+function MetaPixel() {
   return (
+    <Helmet>
+      <script>{`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '573384189084006');
+        fbq('track', 'PageView');
+      `}</script>
+      <noscript>{`
+        <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=573384189084006&ev=PageView&noscript=1"
+        />
+      `}</noscript>
+    </Helmet>
+  );
+}
+
+  return (
+   
     <Router>
+   <MetaPixel />
       <ScrollToTop />
       <div
         style={{
